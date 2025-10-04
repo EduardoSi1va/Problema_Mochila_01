548efd00ccbb1c49d75e75ec40b9b25b779a6a9e
Eduardo <eduardo12cps@gmail.com>
2025-10-04 17:23:11 -0300

    algoritmo de forca bruta

--- implementacoes.c
+++ implementacoes.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
-float forca_bruta(float pesos[], float valores[], int n, int peso_max)
+typedef struct item_ 
+{
+    float peso, valor;
+    float razao;
+    bool escolhido; // Variável para verificar quais itens foram escolhidos na combinação final de saída.
+} ITEM;
+
+float forca_bruta(ITEM itens[], int n, int peso_max)
 {
     int total_combinacoes = (int)pow(2, n); // 2^n combinações possíveis (já que cada item pode ou não ser incluído)
     float melhor_valor = 0;
@@ -17,8 +26,8 @@ float forca_bruta(float pesos[], float valores[], int n, int peso_max)
             // Se o último bit é 1, inclui o item j
             if (comb_atual_binaria % 2 == 1)
             {
-                peso_atual += pesos[j];
-                valor_atual += valores[j];
+                peso_atual += itens[j].peso;
+                valor_atual += itens[j].valor;
             }
             comb_atual_binaria = comb_atual_binaria / 2; // Remove o último bit (divisão inteira por 2)
         }
@@ -27,6 +36,16 @@ float forca_bruta(float pesos[], float valores[], int n, int peso_max)
         if (peso_atual <= peso_max && valor_atual > melhor_valor)
         {
             melhor_valor = valor_atual;
+
+            int melhor_comb_binaria = i; // Atribui o número da melhor combinação encontrada
+            for (int k = 0; k < n; k++)
+            {
+                if (melhor_comb_binaria % 2 == 1) //Caso um item genérico tenha sido escolhido, marca como 1(dentro da mochila)
+                {
+                    itens[k].escolhido = 1;
+                }
+                melhor_comb_binaria = melhor_comb_binaria / 2; // Passa para o próximo bit 
+            }
         }
     }
 
@@ -37,15 +56,33 @@ int main(void)
 {
     int n, peso_max;
     scanf("%d %d", &n, &peso_max);
-    float valores[n], pesos[n];
+    ITEM *itens = malloc(n * sizeof(ITEM));
+
+    if (!itens)
+    {
+        printf("Erro na alocação de memória\n");
+        return 1;
+    }
 
     for (int i = 0; i < n; i++)
     {
-        scanf("%f %f", &valores[i], &pesos[i]);
+        scanf("%f %f", &itens[i].valor, &itens[i].peso);
+        itens[i].razao = itens[i].valor / itens[i].peso;
+        itens[i].escolhido = 0; // Atribui 0 para todos os itens (inicialmente todos estão fora da mochila)
     }
 
-    float valor_forca_bruta = forca_bruta(pesos, valores, n, peso_max);
-    printf("Valor máximo (força bruta): %.2f\n", valor_forca_bruta);
+    float valor_forca_bruta = forca_bruta(itens, n, peso_max);
 
+    printf("\nLista de itens escolhidos:\n");
+    for (int j = 0; j < n; j++)
+    {
+        if (itens[j].escolhido)
+        {
+            printf("%dº: Valor = %.2f, Peso = %.2f\n", j + 1, itens[j].valor, itens[j].peso);
+        }
+    }
+    printf("\nValor máximo (força bruta): %.2f\n", valor_forca_bruta);
+
+    free(itens);
     return 0;
 }
